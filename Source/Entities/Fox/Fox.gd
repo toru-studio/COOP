72fc1821d3b70d1bd09d9759856cab5c6dbe261c
Finlay Metcalfe <43369124+Ofthemasses@users.noreply.github.com>
2024-01-19 22:16:25 +1300

    Refactors and Fox Spawner Implemented

--- Source/Entities/Fox/Fox.gd
+++ Source/Entities/Fox/Fox.gd
@@ -5,7 +5,7 @@ var path_end : float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.progress_ratio = randf()
+	self.progress_ratio = 0.0
 	path_end = .985
 	pass # Replace with function body.
 
@@ -16,5 +16,5 @@ func _process(delta):
 	self.progress_ratio += direction * speed * delta
 	
 	if self.progress_ratio > path_end:
-		self.progress_ratio = 0.0
+		self.free()
 	pass

04b9b4178baac563fa90a095c16ed76ec86e6601
Finlay Metcalfe <43369124+Ofthemasses@users.noreply.github.com>
2024-01-20 11:32:33 +1300

    Abstract fox class implemented

--- Source/Entities/Fox/Fox.cs
+++ Source/Entities/Fox/Fox.cs
@@ -1,26 +1,75 @@
 using System;
+using COOP.Source.Entities.Fox;
 using Godot;
 
-public partial class Fox : PathFollow2D
+public abstract partial class Fox : PathFollow2D
 {
-	private readonly float PATH_END = 0.985f;
+	private const float PATH_END = 0.985f;
+	private const float PATH_START = 0.05f;
+
+	protected float NormalSpeed;
+	protected float BlindSpeed;
+	protected float FleeSpeed;
+
+	protected int BlindLevel;
+	protected double ElapsedBlindness;
+	protected double MaxElapsedBlindness;
+	protected double ElapsedRecovery;
+	protected double MaxElapsedRecovery;
+	protected FoxState State;
 	
-	private float speed;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		this.ProgressRatio = 0.0f;
-		this.speed = 0.2f;
+		this.ElapsedBlindness = 0.0;
+		this.ElapsedRecovery = 0.0;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		this.ProgressRatio += speed * (float)delta;
-		GD.Print("TEST");
+		switch (State)
+		{
+			case FoxState.NORMAL:
+				this.Normal(delta);
+				break;
+			case FoxState.BLIND:
+				this.Blind(delta);
+				break;
+			case FoxState.FLEE:
+				this.Flee(delta);
+				break;
+		}
+	}
+
+	private void Normal(double delta)
+	{
+		this.ProgressRatio += NormalSpeed * (float)delta;
 		if (this.ProgressRatio > PATH_END)
+		{
+			this.Free();
+        }
+		if (BlindLevel >= 100)
+        {
+			this.State = FoxState.BLIND;
+        }
+	}
+
+	private void Blind(double delta)
+	{
+		this.ProgressRatio += BlindSpeed * (float)delta;
+	}
+
+	private void Flee(double delta)
+	{
+		this.ProgressRatio += FleeSpeed * (float)delta;
+		if (this.ProgressRatio < PATH_END)
 		{
 			this.Free();
 		}
 	}
+	
+	public abstract void ShineOn(double strength);
 }

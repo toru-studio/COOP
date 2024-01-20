06e5d0dfa84ce2f6e08cd505eeedf48277632e55
Finlay Metcalfe <43369124+Ofthemasses@users.noreply.github.com>
2024-01-20 13:39:54 +1300

    Setup currency label and hooked it into a variable

--- Source/Game/States/TDState/Game.cs
+++ Source/Game/States/TDState/Game.cs
@@ -3,21 +3,36 @@ using System;
 
 public partial class Game : Node2D
 {
-	private FoxSpawner spawner;
+	private FoxSpawner Spawner;
+	private Label CurrencyLabel;
+	private int Currency;
 
 	private double test_elapsed;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		test_elapsed = 0.0;
+		
+		// Link Nodes
 		try
 		{
-			this.spawner = (FoxSpawner)this.FindChild("Pathing").FindChild("Path2D");
+			this.Spawner = (FoxSpawner)this.FindChild("Pathing").FindChild("Path2D");
 		}
 		catch (Exception e)
 		{
 			GD.Print("Fox Spawner Not Found");
 		}
+
+		try
+		{
+			this.CurrencyLabel = (Label)this.FindChild("Currency");
+		}
+		catch (Exception e)
+		{
+			GD.Print("Currency Label Not Found");
+		}
+
+		this.CurrencyLabel.Text = "0";
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,8 +41,19 @@ public partial class Game : Node2D
 		test_elapsed += delta;
 		if (test_elapsed > .1)
 		{
-			this.spawner.AddFox(GD.Load<PackedScene>("res://Source/Entities/Fox/fox.tscn"));
+			this.Spawner.AddFox(GD.Load<PackedScene>("res://Source/Entities/Fox/fox.tscn"));
 			test_elapsed = 0.0;
 		}
+	}
+
+	public int GetCurrency()
+	{
+		return Currency;
+	}
+
+	public void AddCurrency(int value)
+	{
+		Currency += value;
+		CurrencyLabel.Text = Convert.ToString(Currency);
 	}
 }

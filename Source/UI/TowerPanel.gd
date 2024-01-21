e64b9232b6a8e798821f31ceda6545ce8b261d1f
bridgenath1 <bridgenath1@myvuw.ac.nz>
2024-01-21 22:19:30 +1300

    Generalised the handling of the Tower Docks dragging

--- Source/UI/TowerPanel.gd
+++ Source/UI/TowerPanel.gd
@@ -1,27 +1,29 @@
 extends Panel
 
 var currImageCopy 
-@onready var lightTower = preload("res://Source/Entities/Towers/light_tower.tscn")
+var towerScene
 
+func _on_ready():
+	var towerType = get_child(0).name
+	var towerPath = "res://Source/Entities/Towers/"+towerType+".tscn"
+	towerScene = load(towerPath)
 
 
 func _on_gui_input(event):
-	var tempTower = lightTower.instantiate()
-
+	var tempTower = towerScene.instantiate()
 	if event is InputEventMouseButton and event.button_mask == 1:
 		currImageCopy = get_child(0).duplicate()
 		currImageCopy.global_position = event.global_position
 		get_tree().current_scene.add_child(currImageCopy)
-		print("Tower Spawned")
 	elif event is InputEventMouseMotion and event.button_mask == 1:
 		#Dragging
 		currImageCopy.global_position = event.global_position
-		print("Dragging")
+
 	elif event is InputEventMouseButton and event.button_mask == 0:
 		get_tree().current_scene.add_child(tempTower)
 		tempTower.global_position = event.global_position
-		currImageCopy.queue_free()
+		currImageCopy.queue_free()	
+		
 
-		print("Released")
-		
-		
+
+

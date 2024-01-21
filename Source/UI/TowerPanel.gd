7f5b37b921683e22fde1a02a12afabb6c8707729
Finlay Metcalfe <43369124+Ofthemasses@users.noreply.github.com>
2024-01-22 11:05:27 +1300

    Fixed issue where interacting with dock crashes game

--- Source/UI/TowerPanel.gd
+++ Source/UI/TowerPanel.gd
@@ -1,25 +1,13 @@
 extends Panel
 
 var currImageCopy 
-var lightTowerScene
-var flameTowerScene
-var lightningTowerScene
 var towerScene
-var tempTower
 
 func _on_ready():
-	flameTowerScene = load("res://Source/Entities/Towers/flame_tower.tscn")
-	lightTowerScene = load("res://Source/Entities/Towers/light_tower.tscn")
-	lightningTowerScene = load("res://Source/Entities/Towers/lightning_tower.tscn")
-	print(flameTowerScene)
-	print(lightTowerScene)
-	print(lightningTowerScene)
-	
+	var towerName = get_child(0).name
+	towerScene = load("res://Source/Entities/Towers/"+towerName+".tscn")
 	
 func _on_gui_input(event):
-	tempTower =  lightTowerScene.instantiate()
-	print("Tower scene:", towerScene)
-
 	if event is InputEventMouseButton and event.button_mask == 1:
 		currImageCopy = get_child(0).duplicate()
 		currImageCopy.global_position = event.global_position
@@ -29,9 +17,10 @@ func _on_gui_input(event):
 		currImageCopy.global_position = event.global_position
 
 	elif event is InputEventMouseButton and event.button_mask == 0:
-		get_tree().current_scene.add_child(tempTower)
-		tempTower.global_position = event.global_position
-		currImageCopy.queue_free()	
+		var tower = towerScene.instantiate()
+		get_tree().current_scene.add_child(tower)
+		tower.global_position = event.global_position
+		currImageCopy.queue_free()
 	
 
 #func _on_gui_input(event):
